7a65ebceff2a703a735655165d45df757eb31c88
djbb7 <danielbruzual@gmail.com>
2015-12-05 11:11:34 +0100

    fix port/hostname for heroku

--- src/introsde/assignment/soap/PeoplePublisher.java
+++ src/introsde/assignment/soap/PeoplePublisher.java
@@ -1,11 +1,31 @@
 package introsde.assignment.soap;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.xml.ws.Endpoint;
 
 public class PeoplePublisher {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
 		System.out.println("Starting SOAP server...");
-		Endpoint.publish("http://localhost:6903/ws/people", new PeopleImpl());
+		
+		String protocol = "http://";
+        String port_value = "5700";
+        if (String.valueOf(System.getenv("PORT")) != "null"){
+            port_value=String.valueOf(System.getenv("PORT"));
+        }
+        String port = ":"+port_value+"/";
+        String hostname = InetAddress.getLocalHost().getHostAddress();
+        if (hostname.equals("127.0.0.1"))
+        {
+            hostname = "localhost";
+        }
+        
+        URI BASE_URI = new URI(protocol + hostname + port);
+
+		Endpoint.publish(BASE_URI.toString()+"/ws/people", new PeopleImpl());
 		System.out.println("SOAP server started.");
 	}
 }
