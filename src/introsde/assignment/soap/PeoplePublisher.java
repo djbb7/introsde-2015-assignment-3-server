a556b31c69d383ca0c4883502c325775e61524b4
djbb7 <danielbruzual@gmail.com>
2015-12-05 10:51:15 +0100

    Add message when listening for connection

--- src/introsde/assignment/soap/PeoplePublisher.java
+++ src/introsde/assignment/soap/PeoplePublisher.java
@@ -4,6 +4,8 @@ import javax.xml.ws.Endpoint;
 
 public class PeoplePublisher {
 	public static void main(String[] args){
+		System.out.println("Starting SOAP server...");
 		Endpoint.publish("http://localhost:6903/ws/people", new PeopleImpl());
+		System.out.println("SOAP server started.");
 	}
 }
